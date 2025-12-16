b916f1a7b7096eda2537e1c52bdb5a736fe90867
nungmiie95 <pccm3092@gmail.com>
2025-12-16 20:53:45 +0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-    API_URL: '[https://script.google.com/macros/s/AKfycbxoPRWUh3DLCZYy-bOhA0Fv2tfaS5-W5U7CeRKMMrDt0d_lcyps9CrRzexpKr2e_dQ/exec](https://script.google.com/macros/s/AKfycbxoPRWUh3DLCZYy-bOhA0Fv2tfaS5-W5U7CeRKMMrDt0d_lcyps9CrRzexpKr2e_dQ/exec)',
-    // ...
-};
