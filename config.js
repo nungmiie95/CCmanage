4d4a2a78529da80ef33f29f5ff3f8549c6d1be5c
nungmiie95 <pccm3092@gmail.com>
2025-12-16 20:22:12 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    API_URL: '[https://script.google.com/macros/s/AKfycbxoPRWUh3DLCZYy-bOhA0Fv2tfaS5-W5U7CeRKMMrDt0d_lcyps9CrRzexpKr2e_dQ/exec](https://script.google.com/macros/s/AKfycbxoPRWUh3DLCZYy-bOhA0Fv2tfaS5-W5U7CeRKMMrDt0d_lcyps9CrRzexpKr2e_dQ/exec)',
+    // ...
+};
